3ecc57c6ac0a1ca836ab31a706ebf4851ab548fa
Andy <ajiang10224@gmail.com>
2021-05-12 19:08:42 -0400

    Commit message

--- psi4/src/psi4/libfmm/multipoles_helper.h
+++ psi4/src/psi4/libfmm/multipoles_helper.h
@@ -4,6 +4,7 @@
 #include "psi4/pragma.h"
 #include <functional>
 #include <memory>
+#include <tuple>
 #include <vector>
 
 namespace psi {
@@ -11,10 +12,19 @@ namespace psi {
 class RealSolidHarmonics {
 
     protected:
+      // Ylm[l][m] = sum (coeff * x^a * y^b * z^c), stores a tuple of (coeff, a, b, c)
+      std::vector<std::vector<std::vector<std::tuple<double, int, int, int>>>> mpole_terms_;
       // Real Solid Harmonics, normalized according to Stone's convention
       std::vector<std::vector<double>> Ylm_;
       // Maximum angular momentum
       const int lmax_;
+      // Compute regular harmonics terms (mpole_terms)
+      void compute_regular_terms();
+      // Compute irregular harmonics terms (mpole_terms)
+      void compute_irregular_terms();
+      // Compute multipoles if it were regular
+      
+      // Compute multipoles if it were irregular
 
     public:
       // Constructor
@@ -22,12 +32,17 @@ class RealSolidHarmonics {
 
       // Adds two harmonics together
       void add(const RealSolidHarmonics& rsh);
-      // Compute regular harmonics for an orbital
-      def compute_regular();
-
+      // Compute multipoles from terms
+      virtual void compute_mpoles();
+      // Translate the solid harmonics
+      virtual void translate();
 
 
 }; // End RealSolidHarmonics class
+
+class RealRegularHarmonics : public RealSolidHarmonics {
+
+}
 
 } // namespace psi
 
