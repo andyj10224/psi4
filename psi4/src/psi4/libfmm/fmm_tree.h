33e00a3ddb7b887780a63e21899a966f6ddeef49
Andy <ajiang10224@gmail.com>
2021-05-13 16:57:31 -0400

    Rough Draft of Header Files

---
+++ psi4/src/psi4/libfmm/fmm_tree.h
@@ -0,0 +1,99 @@
+#ifndef libfmm_fmm_tree_H
+#define libfmm_fmm_tree_H
+
+#include "psi4/pragma.h"
+
+#include "psi4/libmints/vector3.h"
+#include "psi4/libmints/matrix.h"
+#include "psi4/libmints/molecule.h"
+#include "psi4/libmints/basisset.h"
+#include "psi4/libfmm/multipole_helper.h"
+
+#include <functional>
+#include <memory>
+#include <tuple>
+#include <vector>
+
+namespace psi {
+
+class CFMMBox {
+
+    protected:
+      // Parent of the CFMMBox
+      std::shared_ptr<CFMMBox> parent_;
+      // Children of the CFMMBox
+      std::vector<std::shared_ptr<CFMMBox>> children_;
+      // Level the box is at (0 = root)
+      int level_;
+      // Maximum Multipole Angular Momentum
+      int lmax_;
+      // Well-separatedness criteria for the particular Box
+      int ws_;
+      // The molecule that is referenced by this box
+      std::shared_ptr<Molecule> molecule_;
+      // The atoms in the molecule which are centered within the bounds of the box
+      std::vector<int> atoms_;
+      // Length of the box
+      double length_;
+      // The box's origin (lower-left-front corner)
+      Vector3 origin_;
+      // Center of the box
+      Vector3 center_;
+
+      // Multipoles of the box
+      std::shared_ptr<RealSolidHarmonics> mpoles_;
+      // Far field vector of the box
+      std::shared_ptr<RealSolidHarmonics> Vff_;
+
+      // A list of all the near-field boxes to this box
+      std::vector<std::shared_ptr<CFMMBox>> near_field_;
+      // A list of all of the local-far-field boxes to this box
+      std::vector<std::shared_ptr<CFMMBox>> local_far_field_;
+
+      // Sets the near field and local far field vectors
+      void set_nf_lff();
+      // Make children for this multipole box
+      void make_children();
+      // Calculates multipoles directly (only for lowest level boxes)
+      void calculate_multipoles();
+      // Calculates the multipoles of a box from it's children
+      void calculate_multipoles_from_children();
+      // Calculates far field vector from local and parent far field vectors
+    public:
+      // Constructor for a root box
+      CFMMBox(std::shared_ptr<Molecule> molecule);
+      // Constructor for child boxes
+      CFMMBox(std::shared_ptr<CFMMBox> parent, std::shared_ptr<Molecule> molecule, Vector3 origin, double length, int level, int lmax);
+
+}; // End class CFMMBox
+
+class CFMMTree {
+
+    protected:
+      // Number of Levels in the CFMM Tree
+      int nlevels_;
+      // The molecule that this tree structure references
+      std::shared_ptr<Molecule> molecule_;
+      // The basis set that the molecule uses
+      std::shared_ptr<BasisSet> basisset_;
+      // Maximum Multipole Angular Momentum
+      int lmax_;
+      // Length of root
+      double length_;
+      // Root of this tree structure
+      std::shared_ptr<CFMMBox> root_;
+
+      // Create children
+      void make_children_helper(std::shared_ptr<CFMMBox>& box);
+      // Calculate multipoles
+      void calculate_multipoles_helper(std::shared_ptr<CFMMBox>& box);
+    
+    public:
+      // Constructor
+      CFMMTree(int nlevels, std::shared_ptr<Molecule> molecule);
+
+}; // End class CFMMTree
+
+} // namespace psi
+
+#endif
--- psi4/src/psi4/libfmm/multipoles_helper.h
+++ psi4/src/psi4/libfmm/multipoles_helper.h
@@ -2,12 +2,47 @@
 #define libfmm_mpoles_helper_H
 
 #include "psi4/pragma.h"
+
+#include "psi4/libmints/vector3.h"
+#include "psi4/libmints/matrix.h"
+
 #include <functional>
 #include <memory>
 #include <tuple>
 #include <vector>
 
 namespace psi {
+
+enum SolidHarmonicsType {Regular, Irregular};
+
+class MultipoleRotationFactory {
+
+    protected:
+      Vector3 R_a_;
+      Vector3 R_b_;
+
+      // New Z axis in rotated frame of reference
+      Vector3 Uz_;
+
+      // Cached Rotation Matrices in a vector of Matrices
+      std::vector<SharedMatrix> D_cache_;
+
+      SharedMatrix U(int l, int m, int M);
+      SharedMatrix V(int l, int m, int M);
+      SharedMatrix W(int l, int m, int M);
+      SharedMatrix P(int i, int l, int mu, int M);
+
+      double u(int l, int m, int M);
+      double v(int l, int m, int M);
+      double w(int l, int m, int M);
+
+    public:
+      // Constructor
+      MultipoleRotationFactory(Vector3 R_a, Vector3 R_b);
+
+      SharedMatrix get_D();
+
+}; // End MultipoleRotationFactory
     
 class RealSolidHarmonics {
 
@@ -18,31 +53,39 @@ class RealSolidHarmonics {
       std::vector<std::vector<double>> Ylm_;
       // Maximum angular momentum
       const int lmax_;
-      // Compute regular harmonics terms (mpole_terms)
-      void compute_regular_terms();
-      // Compute irregular harmonics terms (mpole_terms)
-      void compute_irregular_terms();
+      // Regular or Irregular?
+      SolidHarmonicsType type_;
+
+      // Compute terms if it were regular
+      void compute_terms_regular();
+      // Compute terms if it were irregular
+      void compute_terms_irregular();
+
       // Compute multipoles if it were regular
-      
+      void compute_mpoles_regular();
       // Compute multipoles if it were irregular
+      void compute_mpoles_regular();
+
+      // Translate multipoles if it were regular
+      void translate_regular();
+      // Translate multipoles if it were irregular
+      void translate_irregular();
 
     public:
       // Constructor
-      RealSolidHarmonics(int lmax);
+      RealSolidHarmonics(int lmax, SolidHarmonicsType type);
 
       // Adds two harmonics together
       void add(const RealSolidHarmonics& rsh);
-      // Compute multipoles from terms
-      virtual void compute_mpoles();
-      // Translate the solid harmonics
-      virtual void translate();
 
+      // Compute the terms (regular)
+      void compute_terms();
+      // Compute multipoles from terms
+      void compute_mpoles();
+      // Translate the solid harmonics
+      void translate();
 
 }; // End RealSolidHarmonics class
-
-class RealRegularHarmonics : public RealSolidHarmonics {
-
-}
 
 } // namespace psi
 
